807487ab9f079ee5023f3413a2776aeccc703936
Alexandria L Walker <57812181+AlexaW026@users.noreply.github.com>
2025-11-26 22:39:31 -0500

    Changed period to reflect 10 from Jan 1 2015 to Dec 31 2024; redownloaded CSVs, and deleted WBA.csv

--- download_data.py
+++ download_data.py
@@ -11,7 +11,7 @@ def downloadData():
     for ticker in tickers:
         ticker = ticker.replace(".", "-")
         
-        df = yf.download(ticker, period="5y", interval="1d", auto_adjust=True)
+        df = yf.download(ticker, start="2015-01-01", end="2025-01-01", interval="1d", auto_adjust=True)
         
         file_path = os.path.join(DATA_PATH, f"Ticker_Data/{ticker}.csv")
         df.to_csv(file_path)
