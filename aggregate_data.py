df1f5b8188720dc2d7326a6e0ba652e2075a127d
Alexandria L Walker <57812181+AlexaW026@users.noreply.github.com>
2025-11-28 11:10:37 -0500

    Creatd script to aggregate compounded returns by year, for specified column (Sector or Sub-Industry); script also creates a dir and adds .gitkeep

---
+++ aggregate_data.py
@@ -0,0 +1,43 @@
+import polars as pl
+from pathlib import Path
+
+MASTER_DATA_FILES = list(Path("Data/Master_Data").glob("*.csv"))
+
+
+def save_agg(col):
+    df = master_df()
+    df = add_columns(df)
+    yearly_returns = compounded_yearly_returns(df, col)
+
+    aggregates_path = Path("Data/Aggregated_Data")
+    aggregates_path.mkdir(parents=True, exist_ok=True)
+    (aggregates_path / ".gitkeep").touch(exist_ok=True)
+
+    filename = Path(aggregates_path) / f"{col}_compounded_yearly_returns.csv"
+    yearly_returns.write_csv(filename)
+
+
+def compounded_yearly_returns(master_df, col):
+    yearly_returns =(
+        master_df
+        .group_by(["Year", col])
+        .agg([((1 + pl.col("Daily Return")).cum_prod().last() - 1)
+              .alias("Compounded Return")])
+              .drop_nulls().sort(["Year", col])
+        )
+
+    return yearly_returns
+
+
+def add_columns(master_df):
+    master_df = master_df.with_columns(pl.col("Close")
+                                       .pct_change().over("Ticker")
+                                       .fill_null(0).alias("Daily Return"))
+    
+    master_df = master_df.with_columns(pl.col("Date").dt.year().alias("Year"))
+    
+
+def master_df():
+    dfs = [pl.read_csv(file) for file in MASTER_DATA_FILES]
+    master_df = pl.concat(dfs)
+    master_df = master_df.with_columns(pl.col("Date").str.strptime(pl.Date, "%Y-%m-%d"))
